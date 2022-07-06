1e58c35ee6cbc8d705c5dbace6b858bac37af716
guoweijie <guoweijie@mail.360.cn>
2022-07-06 11:23:54 +0800

    add alacritty config

--- other/setup.sh
+++ other/setup.sh
@@ -1,15 +1,24 @@
-#!/bin/bas#config for fish
 if [ -f ~/.config/fish/config.fish ]; then
   rm ~/.config/fish/config.fish -f
-  mkdir -p ~/.config/fish/
-  ln -s ~/.config/nvim/other/config.fish ~/.config/fish/
 fi
+mkdir -p ~/.config/fish/
+ln -s ~/.config/nvim/other/config.fish ~/.config/fish/
 
 
 #config for tmuxp
-if [ -f ~/.config/tmuxp/cppdebug.yml ]; then
-  rm ~/.config/tmuxp/cppdebug.yml -f
-  mkdir -p ~/.config/tmuxp/
-  ln -s ~/.config/nimv ~/.config/fish/
-  ln -s ~/.config/nvim/other/cppdebug.yml ~/.config/nvim/other/
+if [ -f ~/.config/tmuxp/tm_cppdebug.yml ]; then
+  rm ~/.config/tmuxp/tm_cppdebug.yml -f
 fi
+mkdir -p ~/.config/tmuxp/
+ln -s ~/.config/nvim/other/tm_cppdebug.yml ~/.config/tmuxp/
+
+
+#config for alacritty
+if [ -f ~/.config/alacritty/alacritty.yml ]; then
+  rm ~/.config/alacritty/alacritty.yml -f
+fi
+if [ -f ~/.config/alacritty/alacritty_debug.yml ]; then
+  rm ~/.config/alacritty/alacritty_debug.yml -f
+fi
+mkdir -p ~/.config/alacritty/
+ln -s ~/.config/nvim/other/alacritty_debug.yml ~/.config/alacritty/
