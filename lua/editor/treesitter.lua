ed9524a4b2450b76c60bd517ddcf73de0e34ca24
guoweijie <guoweijie@mail.360.cn>
2022-12-20 18:51:29 +0800

    修改配置

--- lua/editor/treesitter.lua
+++ lua/editor/treesitter.lua
@@ -6,14 +6,14 @@ end
 configs.setup {
 
   -- A list of parser names, or "all"
-  ensure_installed = { "c", "lua", "rust" },
+  ensure_installed = { "c", "lua", "rust", "javascript" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
 
   -- List of parsers to ignore installing (for "all")
-  ignore_install = { "javascript" },
-
+  --[[ ignore_install = { "javascript" }, ]]
+  
   highlight = {
     -- `false` will disable the whole extension
     enable = true,
