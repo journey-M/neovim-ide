627071ddb3c8a25cfc4eaf05e72c47cc6a2ee980
nobody <1485765145@qq.com>
2024-08-12 19:12:11 +0000

    alacritty config

--- other/config.fish
+++ other/config.fish
@@ -1,21 +1,21 @@
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-if test -f ~/.autojump/share/autojump/autojump.fish; . ~/.autojump/share/autojump/autojump.fish; end
 
 #for rust
-set -gx PATH ~/.cargo/bin $PATH
 #for mycmds
 set -gx PATH $PATH ~/.cmds
-set -gx PATH $PATH /opt/flutter/bin/
-set -gx PATH $PATH ~/.local/share/gem/ruby/3.0.0/bin
-set -gx PATH $PATH /home/gwj/Android/Sdk/build-tools/32.0.0
+set -gx PATH $PATH /home/guoweijie/archTools/gn/out/
+set -gx PATH $PATH /home/guoweijie/archTools/depot_tools/
+
 alias gst "git status"
 alias gbr "git branch"
 alias gck "git checkout"
 alias gpo "git push origin"
 alias gcm "git commit -m"
 alias gad "git add ."
+alias j "autojump"
 abbr  n nvim
 abbr  c cd
 abbr  ffplay_small ffplay -x 380 -y 300
